de06cc72cf4dbe1f820fe57b292e6958c21a5653
H2Owater425 <h2o@dhmo.kr>
2025-09-21 19:26:40 +0900

    초기 기록

---
+++ 기호.h
@@ -0,0 +1,90 @@
+#define 끝 ;
+#define 그리고 ,
+
+#define 인수시작 (
+#define 인수끝 )
+#define 인자시작 (
+#define 인자끝 )
+#define 유형시작 (
+#define 유형끝 )
+#define 묶음시작 (
+#define 묶음끝 )
+#define 조건시작 (
+#define 조건끝 )
+
+#define 함수시작 {
+#define 함수끝 }
+#define 상태시작 {
+#define 상태끝 }
+#define 구조체시작 {
+#define 구조체끝 }
+#define 열거형시작 {
+#define 열거형끝 }
+#define 공용체시작 {
+#define 공용체끝 }
+#define 배열시작 {
+#define 배열끝 }
+
+#define 크기시작 [
+#define 크기끝 ]
+#define 색인시작 [
+#define 색인끝 ]
+
+#define 더하기 +
+#define 빼기 -
+#define 나누기 /
+#define 곱하기 *
+#define 나머지 %
+
+#define 음수 -
+
+#define 과같은 ==
+#define 과같지않은 !=
+#define 와같은 ==
+#define 와같지않은 !=
+#define 보다큰 >
+#define 보다작은 <
+#define 보다크거나같은 >=
+#define 보다작거나같은 <=
+
+#define 논리곱 &&
+#define 논리합 ||
+#define 논리부정 !
+
+#define 두값별논리곱 &
+#define 두값별논리합 |
+#define 두값별배타적논리합 ^
+#define 두값별논리부정 ~
+
+#define 두값별왼쪽밀기 <<
+#define 두값별오른쪽밀기 >>
+
+#define 할당 =
+#define 더하고할당 +=
+#define 빼고할당 -=
+#define 곱하고할당 *=
+#define 나누고할당 /=
+#define 나머지할당 %=
+
+#define 두값별논리곱하고할당 &=
+#define 두값별논리합하고할당 |=
+#define 두값별배타적논리합하고할당 ^=
+#define 두값별왼쪽밀고할당 <<=
+#define 두값별오른쪽밀고할당 >>=
+
+#define 증가 ++
+#define 감소 --
+
+#define 참이면 ?
+#define 아니면 :
+
+#define 참조 &
+#define 역참조 *
+#define 의주소 *
+
+#define 소수점 .
+
+#define 접근 .
+#define 역참조하고접근 ->
+
+#define 줄임 ...
